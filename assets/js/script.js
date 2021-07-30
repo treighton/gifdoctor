0185cf1ef82d8f0d751190d21e0d96573d29a6ee
Ryan Skog <ryan.skog@outlook.com>
2021-07-29 19:43:10 -0700

    Removed all comments and added one

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,42 @@
-var usersContainer = document.getElementById('users');
-var fetchButton = document.getElementById('fetch-button');
+var usersContainer = document.getElementById("users");
+var fetchButton = document.getElementById("fetch-button");
 
+// This script looks legit
 
-function getgif(search){
-
-  const key = `q4pEuS5Ltps9oP7m7gPiww7Jdn4fDK71`
-  const url = `https://api.giphy.com/v1/gifs/search?api_key=${key}&q=${search}`
+function getgif(search) {
+  const key = `q4pEuS5Ltps9oP7m7gPiww7Jdn4fDK71`;
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=${key}&q=${search}`;
 
   fetch(url)
-    .then(function(resp){ 
-      if( resp.status !== 200 ) {
-        return console.log('shits broke')
+    .then(function (resp) {
+      if (resp.status !== 200) {
+        return console.log("shits broke");
       }
 
-      return resp.json() 
+      return resp.json();
     })
-    .then(function( { data } ) {
-      console.log(data)
-      const imgs = data.map(function(item){
-        return `<img class="result-img" src="${item.images.original.url}" >`
-      } )
+    .then(function ({ data }) {
+      console.log(data);
+      const imgs = data.map(function (item) {
+        return `<img class="result-img" src="${item.images.original.url}" >`;
+      });
 
-      console.log(imgs)
-      usersContainer.innerHTML = imgs.join('')
+      console.log(imgs);
+      usersContainer.innerHTML = imgs.join("");
     })
-    .catch(function(err){
-      console.error(err)
-    })
-
+    .catch(function (err) {
+      console.error(err);
+    });
 }
 
 function getWeather(search) {
-  const key = '9fecab4e5a8c3a63bd65cf759f919173'
-  const url = `https://api.openweathermap.org/data/2.5/forecast?q=${search}&appid=${key}&units=imperial`
+  const key = "9fecab4e5a8c3a63bd65cf759f919173";
+  const url = `https://api.openweathermap.org/data/2.5/forecast?q=${search}&appid=${key}&units=imperial`;
 
   fetch(url)
-    .then(resp => resp.json())
-    .then(data => {
-      console.log(data)
+    .then((resp) => resp.json())
+    .then((data) => {
+      console.log(data);
       const card = `
       <div class="card">
         <h1>${data.name}</h1>
@@ -47,34 +46,32 @@ function getWeather(search) {
           <li>Wind: ${data.wind.speed} Direction:${data.wind.deg} </li>
         </ul>
       </div>
-      `
-      usersContainer.innerHTML = card
-    })
+      `;
+      usersContainer.innerHTML = card;
+    });
 }
 
-function getLOC(search){
-    const url = `https://www.loc.gov/notated-music/?q=${search}&fo=json`
-    fetch(url)
-    .then(function(resp){ 
-      if( resp.status !== 200 ) {
-        return console.log('shits broke')
+function getLOC(search) {
+  const url = `https://www.loc.gov/notated-music/?q=${search}&fo=json`;
+  fetch(url)
+    .then(function (resp) {
+      if (resp.status !== 200) {
+        return console.log("shits broke");
       }
 
-      return resp.json() 
+      return resp.json();
     })
-    .then(function(data) {
-      console.log(data.results)
-      data.results.forEach(element => {
+    .then(function (data) {
+      console.log(data.results);
+      data.results.forEach((element) => {
         console.log(element.title);
       });
     })
-    .catch(function(err){
-      console.error(err)
-    })
+    .catch(function (err) {
+      console.error(err);
+    });
 }
 
-
-
-fetchButton.addEventListener('click', function(){
-  getLOC(document.querySelector('#search').value)
+fetchButton.addEventListener("click", function () {
+  getLOC(document.querySelector("#search").value);
 });
